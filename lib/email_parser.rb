578bb61b57ee37f47eaac8117c31e5b81d212d4e
John Troutman <jtroutmanman@gmail.com>
2020-10-02 21:14:57 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,7 +12,7 @@ class EmailAddressParser
   
   def parse
     if @email_addresses.include?(",")
-       @email_addresses.split(",").strip
+       @email_addresses.split(", ").strip
     elsif @email_addresses.include?(" ")
        @email_addresses.split(" ")
      end
